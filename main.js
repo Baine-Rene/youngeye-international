3e4e75d0d9e0f28dfaf3dd72172723aaa3be5b7d
Rene Baine <renebaine13@gmail.com>
2024-01-27 20:17:54 +0300

    landing page updates

--- main.js
+++ main.js
@@ -1,15 +1,62 @@
  // Initiate the wowjs
     new WOW().init();
 
- // Back to top button
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 300) {
-                $('.back-to-top').fadeIn('slow');
+    window.addEventListener('DOMContentLoaded', event => {
+
+        // Navbar shrink function
+        var navbarShrink = function () {
+            const navbarCollapsible = document.body.querySelector('#mainNav');
+            if (!navbarCollapsible) {
+                return;
+            }
+            if (window.scrollY === 0) {
+                navbarCollapsible.classList.remove('navbar-shrink')
             } else {
-                $('.back-to-top').fadeOut('slow');
+                navbarCollapsible.classList.add('navbar-shrink')
+            }
+    
+        };
+    
+        // Shrink the navbar 
+        navbarShrink();
+    
+        // Shrink the navbar when page is scrolled
+        document.addEventListener('scroll', navbarShrink);
+    
+        // Activate Bootstrap scrollspy on the main nav element
+        const mainNav = document.body.querySelector('#mainNav');
+        if (mainNav) {
+            new bootstrap.ScrollSpy(document.body, {
+                target: '#mainNav',
+                offset: 74,
+            });
+        };
+    
+        // Collapse responsive navbar when toggler is visible
+        const navbarToggler = document.body.querySelector('.navbar-toggler');
+        const responsiveNavItems = [].slice.call(
+            document.querySelectorAll('#navbarToggler .nav-link')
+        );
+        responsiveNavItems.map(function (responsiveNavItem) {
+            responsiveNavItem.addEventListener('click', () => {
+                if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                    navbarToggler.click();
+                }
+            });
+        });
+    
+    });
+
+    document.addEventListener("DOMContentLoaded", function () {
+        var icon = document.getElementById("navbarIcon");
+
+        document.querySelector('.navbar-toggler').addEventListener('click', function () {
+            if (icon.classList.contains('fa-bars')) {
+                icon.classList.remove('fa-bars');
+                icon.classList.add('fa-times');
+            } else {
+                icon.classList.remove('fa-times');
+                icon.classList.add('fa-bars');
             }
         });
-        $('.back-to-top').click(function () {
-            $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-            return false;
-        });
+    });
